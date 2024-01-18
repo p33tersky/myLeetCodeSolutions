2bf0288f6573afd53c7f9751c9f3823b6feb3f22
Piotr <97466082+p33tersky@users.noreply.github.com>
2024-01-18 22:45:37 +0100

    Time: 1980 ms (5.00%) | Memory: 40.4 MB (19.09%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.java
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -1,12 +1,6 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-         int half;
-         if(num%2==1){
-             half = (num+1)/2;
-         } else {
-             half = num/2;
-         }
-         for(int i = 0; i<=half; i++){
+         for(int i = 0; i<=(num+1)/2; i++){
              if (i*i==num){
                  return true;
              }
