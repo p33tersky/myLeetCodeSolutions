38dc6346d58c67b82b4275196f6e4164de700984
Piotr <97466082+p33tersky@users.noreply.github.com>
2024-01-28 00:07:40 +0100

    Time: 1 ms (53.01%) | Memory: 41 MB (14.98%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,10 +1,6 @@
 class Solution {
       public  boolean isPowerOfFour(int n) {
-        if (n <= 0) {
-            return false;
-        }
         double log = Math.log(n)/Math.log(4);
-        int logAtInd0 = Integer.parseInt(String.valueOf(String.valueOf(log).split("\\.")[0]));
-        return Math.pow(4, logAtInd0) == n;
+        return n>0 && log == (int) log;
     }
 }
