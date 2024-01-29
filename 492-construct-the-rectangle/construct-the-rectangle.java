c4c2d391f0c1b87b87d603ed04b3574646b4f82c
Piotr <97466082+p33tersky@users.noreply.github.com>
2024-01-29 20:18:33 +0100

    Time: 0 ms (100.00%) | Memory: 41 MB (30.00%) - LeetSync

--- 492-construct-the-rectangle/construct-the-rectangle.java
+++ 492-construct-the-rectangle/construct-the-rectangle.java
@@ -1,19 +1,13 @@
 class Solution {
-    public static int[] constructRectangle(int area) {
-        List<Integer> dividers = new ArrayList<>();
-        for (int i = area; i > 0 ; i--) {
+    public int[] constructRectangle(int area) {
+        int array[] = new int[2];
+        for (int i = (int) Math.sqrt(area); i>0; i--){
             if (area%i==0){
-                dividers.add(i);
+                array[0] = area/i;
+                array[1] = i;
+                break;
             }
         }
-        int[] array = new  int[2];
-        if (dividers.size()%2==0){
-            array[0] = dividers.get(dividers.size()/2-1);
-            array[1] = dividers.get(dividers.size()/2);
-            return array;
-        }
-        array[0] = dividers.get((dividers.size()-1)/2);
-        array[1] = array[0];
         return array;
     }
 }
