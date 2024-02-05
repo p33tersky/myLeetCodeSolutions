221e9a57566789321e375c0551304bc7f65b54e6
Piotr <97466082+p33tersky@users.noreply.github.com>
2024-02-05 23:10:48 +0100

    Time: 81 ms (25.27%) | Memory: 41.1 MB (95.10%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -3,22 +3,22 @@ class Solution {
         int k = 1;
         int m = 0;
         List<Integer> squares = new ArrayList<>();
-        while (k < n+1){
+        while (k < n + 1) {
             squares.add(k);
             m++;
-            k +=2*m +1;
+            k += 2 * m + 1;
         }
-        if (squares.size()*squares.size() == n) {
+        if (squares.size() * squares.size() == n) {
             return 1;
         }
         int result = 4;
         for (int i = 0; i < squares.size(); i++) {
             for (int j = 0; j < squares.size(); j++) {
-                if (squares.get(i) + squares.get(j) == n){
+                if (squares.get(i) + squares.get(j) == n) {
                     return 2;
                 }
                 for (int l = 0; l < squares.size(); l++) {
-                    if (squares.get(i)+squares.get(j)+squares.get(l)==n){
+                    if (squares.get(i) + squares.get(j) + squares.get(l) == n) {
                         result = 3;
                     }
                 }
