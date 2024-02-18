6c94b64293fedc4c9889a3b29c35619378caeb96
Piotr <97466082+p33tersky@users.noreply.github.com>
2024-02-19 00:24:55 +0100

    Time: 1489 ms (91.34%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,10 +1,8 @@
 
 SELECT r.contest_id,
-ROUND(100*COUNT(DISTINCT(r.user_id)) / (SELECT COUNT(DISTINCT(user_id)) FROM Users) , 2) AS "percentage"
+ROUND(100*COUNT(DISTINCT(user_id)) / (SELECT COUNT(DISTINCT(user_id)) FROM Users) , 2) AS "percentage"
 FROM Register r
-JOIN Users u
-ON r.user_id = u.user_id
-GROUP BY r.contest_id
+GROUP BY contest_id
 ORDER BY percentage desc, contest_id asc
 
 
