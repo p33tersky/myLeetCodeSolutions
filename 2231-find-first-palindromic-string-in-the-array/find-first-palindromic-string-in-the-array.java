45c6849d749fd7b77251981fd1177b0e03ebacac
Piotr <97466082+p33tersky@users.noreply.github.com>
2024-02-18 15:17:41 +0100

    Time: 3 ms (39.51%) | Memory: 45 MB (36.58%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.java
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.java
@@ -1,7 +1,7 @@
 class Solution {
     private static boolean isPalindrome(String word) {
         char[] letters = word.toCharArray();
-        for (int i = 0; i < letters.length / 2 + 1; i++) {
+        for (int i = 0; i < letters.length / 2 ; i++) {
             if (letters[i] != letters[letters.length - 1 - i]) {
                 return false;
             }
