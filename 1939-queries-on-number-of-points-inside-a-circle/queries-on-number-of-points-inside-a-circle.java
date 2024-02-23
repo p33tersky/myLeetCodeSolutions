3ef11815a64547a0b6e6c8114709d09a54808820
Piotr <97466082+p33tersky@users.noreply.github.com>
2024-02-23 12:56:22 +0100

    Time: 42 ms (27.84%) | Memory: 45 MB (88.38%) - LeetSync

--- 1939-queries-on-number-of-points-inside-a-circle/queries-on-number-of-points-inside-a-circle.java
+++ 1939-queries-on-number-of-points-inside-a-circle/queries-on-number-of-points-inside-a-circle.java
@@ -2,8 +2,8 @@ class Solution {
     public int[] countPoints(int[][] points, int[][] queries) {
         int[] pointsInCircle = new int[queries.length];
         for (int i = 0; i < queries.length; i++) {
-            for (int j = 0; j < points.length; j++) {
-                if (Math.pow(points[j][0] - queries[i][0], 2) + Math.pow(points[j][1] - queries[i][1], 2) <= Math.pow(queries[i][2], 2)) {
+            for (int[] point : points) {
+                if (Math.pow(point[0] - queries[i][0], 2) + Math.pow(point[1] - queries[i][1], 2) <= Math.pow(queries[i][2], 2)) {
                     pointsInCircle[i]++;
                 }
             }
