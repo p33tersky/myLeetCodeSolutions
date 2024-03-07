95f0684f3d0ab37a27973e82a5deb33252252b82
Piotr <97466082+p33tersky@users.noreply.github.com>
2024-03-07 12:41:55 +0100

    Time: 2 ms (97.99%) | Memory: 45.2 MB (11.47%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,28 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-         int[] tab = new int[2];
-        int pos = 0;
-        int[] tab2 = new int[2];
-        int pos2 = 0;
-        for (int i = 0; i < nums.length ; i++) {
-            int difference = target - nums[i];
-            if (Arrays.stream(nums).filter(n -> n == difference).findAny().isPresent()){
-                if (difference==nums[i]){
-                    tab2[pos2] = i;
-                    pos2++;
-                }else {
-                    tab[pos] = i;
-                    pos++;
-                }
+        Map<Integer, Integer> indices = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int key = target - nums[i];
+            if (indices.get(nums[i]) == null) {
+                indices.put(key, i);
+            } else {
+                return new int[] { i, indices.get(nums[i]) };
             }
-            if (pos > 1) {
-                break;
-            }
-
         }
-        if (tab[0] == 0 && tab[1] ==0){
-            return tab2;
-        }
-        return tab;
+        return nums;
     }
 }
